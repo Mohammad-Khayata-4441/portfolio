9d6c8870fa8476627ac9e2a35d778bfd66570540
mohammad khayata <mohammadkhayta.gm@gmail.com>
2023-02-22 23:42:11 +0300

    deploy options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 export default defineNuxtConfig({
   css: ['~/assets/scss/fonts.scss', '~/assets/scss/reusable.scss', '~/assets/scss/global.scss'],
   ssr: false,
-  app:{
+  app: {
     pageTransition: {
       name: 'page',
       mode: 'out-in' // default
     },
+    baseURL: '/portfolio/' // baseURL: '/<repository>/'
   },
-
 
 
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
