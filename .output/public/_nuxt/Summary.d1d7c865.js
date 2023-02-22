82e82706d3bd417cc6374f13adb9167b25aacb73
mohammad khayata <mohammadkhayta.gm@gmail.com>
2023-02-23 00:12:28 +0300

    output

---
+++ .output/public/_nuxt/Summary.d1d7c865.js
@@ -0,0 +1,1 @@
+import{i as e,e as r,o as c}from"./entry.07f46b2f.js";const n={};function o(t,a){return c(),r("h1",null,"Summary")}const m=e(n,[["render",o]]);export{m as default};
---
+++ .output/public/_nuxt/composables.2fd1dd1f.js
@@ -0,0 +1,1 @@
+import{h as r}from"./entry.07f46b2f.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
