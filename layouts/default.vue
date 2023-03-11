1871385356c7ad8d0ad6901826c280109f3a7016
mohammad khayata <mohammadkhayta.gm@gmail.com>
2023-03-11 23:38:12 +0300

    text center

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!isMounted"
+  <div v-if="true"
     class="loading -z-10 bg-dark fixed top-0 left-0 h-screen w-screen flex items-center justify-center flex-col">
     <svg class="light blur-[200px] absolute left-0 top-0" width="520" height="396" viewBox="0 0 1020 696" fill="none"
       xmlns="http://www.w3.org/2000/svg">
@@ -13,7 +13,7 @@
         transform="matrix(0.714449 0.699688 0.699688 -0.714449 -106.936 158.962)" fill="#FB1ADE" fill-opacity="0.77" />
     </svg>
 
-    <h1 class="text-3xl text-white">Welcome To Mohammad Khayata Portfolio <span class="text-primary text-4xl"> :) </span> </h1>
+    <h1 class="text-3xl text-center text-white">Welcome To Mohammad Khayata Portfolio <span class="text-primary text-4xl"> :) </span> </h1>
     <h4 class="my-2 text-lg">Loading ... </h4>
   </div>
   <template v-else>
