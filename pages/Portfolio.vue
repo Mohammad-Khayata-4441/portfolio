9521aa596012b52f43764f3cc3e026a2ed771070
mohammad khayata <mohammadkhayta.gm@gmail.com>
2023-04-11 11:07:10 +0300

    Fix bug

--- pages/Portfolio.vue
+++ pages/Portfolio.vue
@@ -2,12 +2,13 @@
     <div class="page portfolio-page  bg-opacity-5">
 
         <div class="page-overlay  flex-grow-1 min-h-full"></div>
-  
+
         <!-- <div class="page-overlay flex-grow-1"></div> -->
 
 
         <div class="container  md:max-w-screen-lg 2xl:max-w-screen-2xl px-4 mx-auto space-y-12">
-            <PortfolioItem class="items-center "  data-aos-duration="1000" v-for="(item, i) in projects?.sort((a , b)=>a.order>b.order?1:-1)" :key="item.id" :item="item">
+            <PortfolioItem v-memo class="items-center " data-aos-duration="1000"
+                v-for="(item, i) in projects?.sort((a, b) => a.order > b.order ? 1 : -1)" :key="item.id" :item="item">
             </PortfolioItem>
         </div>
     </div>
@@ -22,11 +23,7 @@ import ProjectsList from '~/data/projects'
 
 import "aos/dist/aos.css";
 import AOS from "aos";
-const projects = ref<Project[]>()
-onMounted(() => {
-    AOS.init();
-    projects.value = ProjectsList as Project[]
-})
+const projects = computed(() => ProjectsList.sort((a, b) => a.order > b.order ? 1 : -1))
 
 
 useHead({
