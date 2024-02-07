51242a1086d730cf5b5f818e41cef0cf00e09a72
Mohammad Khayata <mohammadkhayta.gm@gmail.com>
2024-02-08 02:14:02 +0300

    ..

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap():Promise<MetadataRoute.Sitemap>{
+
+    return [
+        {
+            url:`https://mohammad-khayata.vercel.app/en/`,
+        },
+        {
+            url:`https://mohammad-khayata.vercel.app/en/about`,
+        },
+        {
+            url:`https://mohammad-khayata.vercel.app/en/portfolio`,
+        },
+    ]
+}
