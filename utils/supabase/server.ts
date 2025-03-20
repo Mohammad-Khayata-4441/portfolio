5bd697bf8d623e12791ec409cda1d0d90162501d
Mohammad-Khayata-4441 <mohammadkhayata.gm@gmail.com>
2025-03-20 20:18:26 +0300

    text

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -4,14 +4,14 @@ import { cookies } from 'next/headers'
 export async function createClient() {
     const cookieStore = await cookies()
 
-    console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
+    console.log(process.env.SUPABASE_URL)
 
 
 
-    console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+    console.log(process.env.SUPABASE_ANON_KEY)
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_ANON_KEY!,
         {
             cookies: {
                 getAll() {
